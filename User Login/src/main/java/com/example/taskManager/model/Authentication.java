d92d991ea319b3c49a94c55fb7e441de99af42be
Hari Haran J <iamharijey@gmail.com>
2022-10-14 14:40:02 +0530

    Add files via upload

---
+++ User Login/src/main/java/com/example/taskManager/model/Authentication.java
@@ -0,0 +1,9 @@
+package com.example.taskManager.model;
+
+import lombok.Data;
+
+@Data
+public class Authentication {
+    private String email;
+    private String password;
+}
---
+++ User Login/src/main/java/com/example/taskManager/model/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.example.taskManager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String status;
+    private String message;
+    private String role;
+}
